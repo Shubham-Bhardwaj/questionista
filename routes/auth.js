a9d37f26ce4b91bbb29b2f73a2597c7d5474dc92
Shubham Bhardwaj <shubhambhardwaj186@gmail.com>
2021-10-16 00:43:42 +0530

    auth route handling

---
+++ routes/auth.js
@@ -0,0 +1,11 @@
+const path = require("path");
+const express = require("express");
+const authControllers = require("../controllers/auth");
+
+const router = express.Router();
+
+router.post("/signup", authControllers.postSignup);
+
+router.post("/login", authControllers.postLogin);
+
+module.exports = router;
--- utils/DBUtils.js
+++ utils/DBUtils.js
@@ -2,9 +2,10 @@ const mysql = require("mysql2/promise");
 require("dotenv").config();
 
 const pool = mysql.createPool({
+  host: "localhost",
   user: process.env.DB_USER,
-  password: process.env.DB_PASSW,
+  password: process.env.DB_PASSWORD,
   database: "questionista",
 });
 
-module.exports = pool
+module.exports = pool;
